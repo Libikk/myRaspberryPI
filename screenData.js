a7d72e64196eed56cd40213956b3e28beafa9df0
Libikk <libikpl@gmail.com>
2020-07-11 11:49:14 +0100

    some changes to screen updated

--- screenData.js
+++ screenData.js
@@ -8,6 +8,10 @@ const credentials = {
 
 let localTemp = '';
 let isRaining = false;
+let lastScreen = 2;
+let internetConnectionStatus = null;
+
+const sendDataIntervalTime = 3000;
 
 const getLightMode = () => {
     var format = 'hh:mm:ss'
@@ -19,15 +23,51 @@ const getLightMode = () => {
     else return 'DARK';
 }
 
-const sendData = async () => {
-    const { temperature, humidity } = await axios.get('http://192.168.0.152/api', { headers: credentials }).then(r => r.data);
-    const lightMode = getLightMode();
-    const line1 = moment().format('YYYY/MM/DD  hh:mm:ss');
-    const line2 = '--Room-----Outside--';
-    const line3 = `  ${humidity.toFixed(2)}  |   ${isRaining ? 'rain' : ''}`;
-    const line4 = `  ${temperature}C |  ${localTemp}C`;
+const getFirstScreenData = async () => {
+  const { temperature, humidity } = await axios.get('http://192.168.0.152/api', { headers: credentials }).then(r => r.data);
+  const line1 = moment().format('YYYY/MM/DD  hh:mm:ss');
+  const line2 = '--Room-----Outside--';
+  const line3 = `  ${humidity.toFixed(2)}  |   ${isRaining ? 'rain' : ''}`;
+  const line4 = `  ${temperature}C |  ${localTemp}C`;
+  return { line1, line2, line3, line4 };
+}
 
-    axios.get(`http://192.168.0.150/screen?line1=${line1}&line2=${line2}&line3=${line3}&line4=${line4}&screenMode=${lightMode}`, { headers: credentials })
+const getInternetConnectionStatus = () => {
+  require('dns').resolve('www.google.com', (err) => {
+    if (err) internetConnectionStatus = 'No connection';
+    else internetConnectionStatus = 'Connected';
+  });
+}
+
+const getSecondScreenData = () => {
+  const line1 = '  Internet status';
+  const line3 = `     ${internetConnectionStatus}`;
+  return { line1, line3 };
+}
+
+const nextScreen = () => {
+  switch(lastScreen) {
+    case 1:
+      lastScreen = 2;
+      break;
+    case 2:
+        lastScreen = 1;
+      break;
+  }
+}
+
+const sendData = async () => {
+
+    let screenData = {}
+    const lightMode = getLightMode();
+    const changeDelay = sendDataIntervalTime;
+    if (lastScreen === 2) screenData = await getFirstScreenData();
+    if (lastScreen === 1) screenData = getSecondScreenData();
+    nextScreen();
+
+    const { line1 = '', line2  = '', line3  = '', line4 = '' } = screenData;
+
+    axios.get(`http://192.168.0.150/screen?line1=${line1}&line2=${line2}&line3=${line3}&line4=${line4}&screenMode=${lightMode}&changeDelay=${changeDelay}`, { headers: credentials })
 }
 
 const getLocalTemperature = () => {
@@ -39,6 +79,7 @@ const getLocalTemperature = () => {
 }
 
 setInterval(getLocalTemperature, 1200000);
+setInterval(getInternetConnectionStatus, 10000);
 getLocalTemperature();
 
-setInterval(sendData, 10000);
+setInterval(sendData, sendDataIntervalTime);
